b73e7a310d014e8df7f508c9727cdc835263081a
concernedsyndicate <futurewasthen@protonmail.ch>
2018-10-16 01:02:18 +0200

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+export (int) var speed  # How fast the player will move (pixels/sec).
+var screensize          # Size of the game window.
+
+func _ready():
+	 screensize = get_viewport_rect().size
+
+func _process(delta):
+    var velocity = Vector2() # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        #$AnimatedSprite.play()
+    #else:
+        #$AnimatedSprite.stop()
