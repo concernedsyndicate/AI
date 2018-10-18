ada34c08ed70b1e4034005f78fd07634ce5a4943
unknown <kobewi4e@gmail.com>
2018-10-18 15:05:40 +0200

    Improved shooting

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,9 +1,14 @@
 extends KinematicBody2D
 
+onready var ray = $Railcast
+
+const RAIL_COOLDOWN = 1.5
+
 export (int) var speed
 export (float) var rotation_speed
 
-var health = 1000 setget set_health
+var health = 100 setget set_health
+var cooldown = 0
 
 var velocity = Vector2()
 var rotation_dir = 0
@@ -11,26 +16,31 @@ var rotation_dir = 0
 func _process(delta):
 	process_move(delta)
 	process_attack(delta)
+	$UI.rotation = -rotation
 
 func process_move(delta):
 	velocity = Vector2(speed * (int(Input.is_action_pressed("up")) - int(Input.is_action_pressed("down"))), 0).rotated(rotation)
 	rotation_dir = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	
 	rotation += rotation_dir * rotation_speed * delta
-	$UI.rotation = -rotation
 	
 	move_and_slide(velocity)
 
 func process_attack(delta):
-	if Input.is_action_just_pressed("shoot"):
+	cooldown -= delta
+	$Sprite.modulate.b = 1 - clamp(cooldown, 0, RAIL_COOLDOWN) / RAIL_COOLDOWN
+	
+	if Input.is_action_just_pressed("shoot") and cooldown <= 0:
+		cooldown = RAIL_COOLDOWN
+		
 		var bullet = preload("res://Nodes/RailgunBullet.tscn").instance()
 		get_parent().add_child(bullet)
 		bullet.position = position
 		bullet.rotation = rotation
 		
-		if $Railcast.is_colliding():
-			$Railcast.get_collider().queue_free()
-			bullet.line.points[1].x = 1
+		if ray.is_colliding():
+			ray.get_collider().queue_free()
+			bullet.line.points[1].x = (global_position - ray.get_collision_point()).length()
 
 func _draw():
 	draw_line(Vector2(0,0), Vector2(speed, 0).rotated(rotation), Color(1, 0, 0, 0.1), 1)
