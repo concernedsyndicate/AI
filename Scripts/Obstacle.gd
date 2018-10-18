273b09d34be148385e8058f650af30d0abcbd2cd
unknown <kobewi4e@gmail.com>
2018-10-18 15:31:08 +0200

    Added map bounds

--- Scripts/Obstacle.gd
+++ Scripts/Obstacle.gd
@@ -4,7 +4,9 @@ extends Node2D
 export var radius = 128
 
 func _ready():
-	if !Engine.editor_hint: $Sprite.scale = Vector2(radius / 128.0, radius / 128.0)
+	if !Engine.editor_hint:
+		$Sprite.scale = Vector2(radius / 128.0, radius / 128.0)
+		$CollisionShape2D.shape.radius = radius
 
 func _process(delta):
 	if Engine.editor_hint:
