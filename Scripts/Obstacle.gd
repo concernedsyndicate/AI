b15fc0babba041c69b84ec193ccdc1f0641a5be6
kobewi <kobewi4e@gmail.com>
2018-10-23 11:09:24 +0200

    Map updates

--- Scripts/Obstacle.gd
+++ Scripts/Obstacle.gd
@@ -1,3 +1,4 @@
+tool
 extends Node2D
 
 export var radius = 128
@@ -7,7 +8,7 @@ func _ready():
 		$Sprite.scale = Vector2(radius / 128.0, radius / 128.0)
 		$CollisionShape2D.shape.radius = radius
 
-func _process(delta):
-	if Engine.editor_hint:
-		$Sprite.modulate.a = 0.1
-		update()
+func _process(delta): if Engine.editor_hint: update()
+
+func _draw():
+	if Engine.editor_hint: draw_circle(Vector2(), radius, Color(0, 1, 0, 0.2))
