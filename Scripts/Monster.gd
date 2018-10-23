58435afe7530394d7c0fbf04eeef88434e949608
concernedsyndicate <futurewasthen@protonmail.ch>
2018-10-23 13:26:00 +0200

    mess in monster
    obstacle avoidance voip

--- Scripts/Monster.gd
+++ Scripts/Monster.gd
@@ -75,6 +75,55 @@ func pursuit(target_position):
 	#now seek to the predicted future position of the evader
 	return seek(target_position + target.velocity * look_ahead_time)
 
+func obstacle_avoidance(obstacles): #przy wywolaniu getTree.getNodesingroup(obstacles)
+
+	#the detection box length is proportional to the agent's velocity
+	m_dDBoxLength = Prm.MinDetectionBoxLength + (speed/max_speed) * Prm.MinDetectionBoxLength
+	
+
+	#tag all obstacles within range of the box for processing
+#	m_pVehicle->World()->TagObstaclesWithinViewRange(m_pVehicle, m_dDBoxLength)
+	
+	#this will keep track of the closest intersecting obstacle (CIB)
+	var closest_intersecting_obstacle
+	
+	#this will be used to track the distance to the CIB
+	double DistToClosestIP = MaxDouble
+	#this will record the transformed local coordinates of the CIB
+	Vector2D LocalPosOfClosestObstacle
+	std::vector<BaseGameEntity*>::const_iterator curOb = obstacles.begin()
+	for obstacle in get_tree().get_nodes_in_group("obstacles"):
+		#if the obstacle has been tagged within range proceed
+		if (obstacle.global_position - global_position).length_squared() < RANGE:
+			#calculate this obstacle's position in local space
+			var LocalPos = Vector2(PointToLocalSpace(obstacle.position, m_pVehicle->Heading(), m_pVehicle->Side(), m_pVehicle->Pos()))
+			#if the local position has a negative x value then it must lay behind the agent. (in which case it can be ignored)
+			if (LocalPos.x >= 0):
+				#if the distance from the x axis to the object's position is less than its radius + half the width of the detection box then there
+				#is a potential intersection.
+				var ExpandedRadius = obstacle.radius + m_pVehicle->BRadius()
+				if (fabs(LocalPos.y) < ExpandedRadius):
+					#now to do a line/circle intersection test. The center of the
+					#circle is represented by (cX, cY). The intersection points are
+					#given by the formula x = cX +/-sqrt(r^2-cY^2) for y=0.
+					#We only need to look at the smallest positive value of x because
+					#that will be the closest point of intersection.
+					double cX = LocalPos.x
+					double cY = LocalPos.y
+					#we only need to calculate the sqrt part of the above equation once
+					double SqrtPart = sqrt(ExpandedRadius*ExpandedRadius - cY*cY)
+					
+					double ip = A - SqrtPart
+					if (ip <= 0):
+						ip = A + SqrtPart
+					#test to see if this is the closest so far. If it is, keep a
+					#record of the obstacle and its local coordinates
+					if (ip < DistToClosestIP):
+						DistToClosestIP = ip
+						ClosestIntersectingObstacle = *curOb
+						LocalPosOfClosestObstacle = LocalPos
+		++curOb
+
 func _draw():
 	draw_set_transform(Vector2(), -rotation, Vector2(1, 1))
 	draw_vector(Vector2(0,0), to_target_heading, Color(0, 0, 1, 0.3), 5)  # blue
